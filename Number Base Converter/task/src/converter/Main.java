3347b4f268916220372a605fdcc089a4bfa3a32d
habibullahturkmen <habibullah_turkmen@yahoo.con>
2021-10-08 01:16:25 +0430

    Initial commit

---
+++ Number Base Converter/task/src/converter/Main.java
@@ -0,0 +1,10 @@
+package converter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner s = new Scanner(System.in);
+    }
+}
